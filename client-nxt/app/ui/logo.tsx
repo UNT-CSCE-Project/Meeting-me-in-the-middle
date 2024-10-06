ab95e1d2547211f8f0409a8e4f99a3e720ec6fa1
Avijeet Shil <avijeetshil110@gmail.com>
2024-10-06 14:13:53 -0500

    SCRUM-40-Manage-Friend-frontend added sidebar

---
+++ client-nxt/app/ui/logo.tsx
@@ -0,0 +1,20 @@
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function Logo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      {/* Using Next.js Image component to reference logo.png in the public folder */}
+      <Image 
+        src="/logo.png"     // Path relative to the public folder
+        alt="Acme Logo" 
+        width={80}          // Set the width of the image
+        height={80
+        }         // Set the height of the image
+      />
+      <p className="text-[20px] px-1 ml-2">Meet Me In The Middle</p>
+    </div>
+  );
+}
