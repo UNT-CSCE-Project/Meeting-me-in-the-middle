515bf6397f1de68b4f80fb406438fe300c25ea92
Avijeet Shil <avijeetshil110@gmail.com>
2024-10-30 21:08:12 -0500

    added interface type

--- client-nxt/app/lib/locationApproval/definitions.ts
+++ client-nxt/app/lib/locationApproval/definitions.ts
@@ -2,8 +2,7 @@ interface placeInfo {
     name: string;
     lat: string | number;
     lng: string | number;
-    formatted_address: string;
     place_id: string;
-    place_type: string;
+    place_type: string[];
     rating: number;
 }
