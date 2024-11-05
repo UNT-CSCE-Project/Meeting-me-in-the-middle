77ca6bb2e4ce4c57f20e5d6e1917452196818be1
Kaylyn King <114537880+iklking@users.noreply.github.com>
2024-11-05 16:47:04 -0600

    uhh idk

--- client-nxt/app/page.tsx
+++ client-nxt/app/page.tsx
@@ -1,29 +1,35 @@
 // page.tsx
 "use client";
-import '@/app/ui/global.css';
-import React from 'react';
-import { usePathname } from 'next/navigation';
-import { useUser } from '@/app/UserContext';
-import { useRouter } from 'next/navigation';
+import "@/app/ui/global.css";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { useUser } from "@/app/UserContext";
+import { useRouter } from "next/navigation";
+import { ThemeProvider } from "./ThemeProvider";
+
 export default function Home() {
-  const {signOutUser, currentUser} = useUser();
+  const { signOutUser, currentUser } = useUser();
   const router = useRouter();
   const pathname = usePathname(); // Get the current route
-  
-  
-  
-  return (
-    <div className="flex h-full flex-col md:flex-row ">
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-        {/* You can add your main content here */}
-        {
-          currentUser ? (
-            <h1 className="text-2xl font-bold">Welcome to Meet Me in the Middle!</h1>            
-          ) : <h1 className="text-2xl font-bold">Please log in to access Meet Me in the Middle!</h1>
-        }
 
-        {/* You can add more content or components here */}
+  return (
+    <ThemeProvider>
+      <div className="flex h-full flex-col md:flex-row ">
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+          {/* You can add your main content here */}
+          {currentUser ? (
+            <h1 className="text-2xl font-bold">
+              Welcome to Meet Me in the Middle!
+            </h1>
+          ) : (
+            <h1 className="text-2xl font-bold">
+              Please log in to access Meet Me in the Middle!
+            </h1>
+          )}
+
+          {/* You can add more content or components here */}
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
