ee9247fa0b39e6feb2e7f8822eaf8aa6a2758e62
Kaylyn King <114537880+iklking@users.noreply.github.com>
2024-11-05 16:37:15 -0600

    idk was starting something but didnt end up finishing it

---
+++ client-nxt/app/ThemeProvider.tsx
@@ -0,0 +1,31 @@
+import React, { useState, createContext, useContext } from 'react';
+
+interface ThemeContextType {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType>({
+  theme: 'light',
+  toggleTheme: () => {},
+});
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const useTheme = () => {
+    return useContext(ThemeContext);
+  };
+
+export { ThemeContext, ThemeProvider, useTheme };
--- client-nxt/app/ui/Navbar.tsx
+++ client-nxt/app/ui/Navbar.tsx
@@ -3,7 +3,8 @@
 import Error from "../ui/error";
 import { lusitana } from "@/app/ui/fonts";
 import { getNotificationCount } from "@/app/lib/notifications/data";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 import { useUser } from "@/app/UserContext";
 import Image from "next/image";
@@ -12,7 +13,7 @@ import {
   ArrowRightEndOnRectangleIcon,
 } from "@heroicons/react/24/outline";
 import ProfileInfo from "./profileInfo";
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { ProfileInfoSkeleton, SearchSkeleton } from "@/app/ui/skeletons";
 import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
@@ -20,6 +21,7 @@ import { set } from "zod";
 import { Notifications } from "./notifications/notifications";
 import Search from "@/app/ui/search";
 import NotificationButton from "@/app/ui/NotificationButton";
+import { useTheme } from "@/app/ThemeProvider";
 
 export default function Navbar() {
   const { signOutUser, userData } = useUser();
@@ -29,13 +31,14 @@ export default function Navbar() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [isPending, setIsPending] = useState(true);
-  
+  const { theme, toggleTheme } = useTheme();
+
   useEffect(() => {
     if (userData && userData.firstName && userData.lastName) {
       console.log(userData);
       setFirstName(userData?.firstName);
       setLastName(userData?.lastName);
-      
+
       setIsPending(false);
     }
   }, [userData]);
@@ -43,25 +46,27 @@ export default function Navbar() {
   return pathname === "/login" || pathname === "/registration" ? (
     <></>
   ) : (
-
     <div className=" bg-[#2c2c2c] px-4">
       <div className="ml-20 mr-4 pt-4">
         <div className="w-25 ml-8 flex items-center justify-between ab">
-          <Suspense fallback={<SearchSkeleton/>}>
+          <button
+            className={`moon-button ${
+              theme === "light" ? "text-black" : "text-white"
+            }`}
+            onClick={toggleTheme}
+          >
+            <FontAwesomeIcon icon={faMoon} size="lg" />
+          </button>
+          <Suspense fallback={<SearchSkeleton />}>
             <Search placeholder="Search for friends" baseUrl="/friends" />
-        
+
             <NotificationButton />
 
             <ProfileInfo firstName={firstName} lastName={lastName} />
-           
-              <button
-                type="button"
-                onClick={signOutUser}
-                className="text-white"
-              >
-                <ArrowRightEndOnRectangleIcon className="h-10 w-10" />
-              </button>
 
+            <button type="button" onClick={signOutUser} className="text-white">
+              <ArrowRightEndOnRectangleIcon className="h-10 w-10" />
+            </button>
           </Suspense>
         </div>
       </div>
