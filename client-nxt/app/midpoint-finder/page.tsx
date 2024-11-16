2a03197bad744af16142dfdd826d40569fad6a7e
Avijeet Shil <avijeetshil110@gmail.com>
2024-11-16 16:10:19 -0600

    fix accessibiliy issues in friends, search mid point finder page

--- client-nxt/app/midpoint-finder/page.tsx
+++ client-nxt/app/midpoint-finder/page.tsx
@@ -178,7 +178,7 @@ function MidpointFinderInner() {
                 fetchFriends()
                 setShowFriendModal(true)}
               }
-              className="text-blue-600 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="text-blue-600 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
             >
               Select Friend
             </button>
